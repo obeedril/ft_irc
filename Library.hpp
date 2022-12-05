b94f4bdf0a1e9e4a25f677d68c298f550e487e69
obeedril <obeedril@student.21-school.ru>
2022-12-05 21:19:23 +0300

    start

---
+++ Library.hpp
@@ -0,0 +1,22 @@
+#ifndef LIBRARY_HPP
+# define LIBRARY_HPP
+
+# include <sys/socket.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <netinet/in.h>
+# include <iostream>
+# include <map>
+# include <list>
+
+
+
+
+
+# include "ChannelsStorage.hpp"
+# include "User.hpp"
+
+
+
+#endif
---
+++ User.hpp
@@ -0,0 +1,34 @@
+#ifndef USER_HPP
+# define USERHPP
+
+# include "Library.hpp"
+
+class User {
+    private:
+        int userFd;
+        bool isAdminHere;
+        std::string channelHere;
+        std::string login;
+        std::string userName;
+        std::string password;
+
+    public:
+        User();
+        ~User();
+        int getUserFd();
+        bool getIsAdminHere();
+        std::string getChannelHere();
+        std::string getLogin();
+        std::string getUserName();
+        std::string getPassword();
+        void setUserFd(int _userFd);
+        void setIsAdminHere(bool _isAdminHere);
+        void setChannelHere(std::string _channelHere);
+        void setLogin(std::string _login);
+        void setUserName(std::string _userName);
+        void setPassword(std::string _password);
+
+};
+
+
+#endif
